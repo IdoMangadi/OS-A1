554cfb8b9db056f77d564fbff88d5248f80651fe
IdoMangadi <idomangadi@gmail.com>
2024-05-02 17:45:11 +0300

    saving3...

---
+++ ex6/add2PB.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main(int argc, int* argv[]){
+
+    // handling arguments:
+    if(argc != 3){
+        fprintf(stderr, "Not enaugh arguments");
+        return 1;
+    }
+
+    
+
+
+
+
+    return 0;
+}
